c4313a0c6a3e8004ae8ea7b3885221e6515b4c88
wangzhao9562 <wangzhao9562@gmail.com>
2020-09-16 09:26:48 +0800

    Modify type errors

--- include/auv_controller/Dynamic.h
+++ include/auv_controller/Dynamic.h
@@ -12,7 +12,7 @@ namespace auv_controller{
      */
     struct AUVDynamic{
         double x_dotu_, y_dotv_, y_dotr_, z_dotw_, z_dotq_;
-        double k_dotq_, m_dotw_, m_dotq_, n_dotv_, n_dotr_;
+        double k_dotp_, m_dotw_, m_dotq_, n_dotv_, n_dotr_;
         double x_uu_, y_vv_, y_rr_, z_ww_, z_qq_;
         double k_pp_, m_ww_, m_qq_, n_vv_, n_rr_;
         double y_uv_, y_ur_, z_uw_, z_uq_;
@@ -40,14 +40,14 @@ namespace auv_controller{
          * @brief AUV dynamic parameters setting
          */
         void setParameters(double xdotu, double ydotv, double ydotr, double zdotw, double zdotq,
-            double kdotq, double mdotw, double mdotq, double ndotv, double ndotr,
+            double kdotp, double mdotw, double mdotq, double ndotv, double ndotr,
             double xuu, double yvv, double yrr, double zww, double zqq,
             double kpp, double mww, double mqq, double nvv, double nrr,
             double yuv, double yur, double zuw, double zuq,
             double muw, double muq, double nuv, double nur)
         {
             x_dotu_ = xdotu; y_dotv_ = ydotv; y_dotr_ = ydotr; z_dotw_ = zdotw; z_dotq_ = zdotq;
-            k_dotq_ = kdotq; m_dotw_ = mdotw; m_dotq_ = mdotq; n_dotv_ = ndotv; n_dotr_ = ndotr;
+            k_dotp_ = kdotp; m_dotw_ = mdotw; m_dotq_ = mdotq; n_dotv_ = ndotv; n_dotr_ = ndotr;
             x_uu_ = xuu; y_vv_ = yvv; y_rr_ = yrr; z_ww_ = zww; z_qq_ = zqq;
             k_pp_ = kpp; m_ww_ = mww; m_qq_ = mqq; n_vv_ = nvv; n_rr_ = nrr;
             y_uv_ = yur; y_ur_ = yur; z_uw_ = zuw; z_uq_ = zuq;
