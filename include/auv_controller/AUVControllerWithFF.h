c11a89ff083654b5462eaa10128a7a45de830aa1
wangzhao9562 <wangzhao9562@gmail.com>
2021-01-29 02:29:22 -0800

    Add auv controller with front fin

---
+++ include/auv_controller/AUVControllerWithFF.h
@@ -0,0 +1,43 @@
+/*                                                                                                         
+ * Filename: AUVControllerWithFF.h
+ * Path: auv_controller
+ * Created Date: Thursday, Janurary 28th 2021, 16:12:39 pm
+ * Author: zhao wang
+ * 
+ * Copyright (c) 2021 Your Company
+ */
+
+#ifndef AUV_CONTROLLER_WITH_FB_H_
+#define AUV_CONTROLLER_WITH_FB_H_
+
+#include "AUVBaseController.h"
+
+namespace auv_controller{
+
+
+/**
+ * @brief AUV slide model controller for model with both of front and back fins
+ */
+class AUVControllerWithFF : public AUVBaseController
+{
+public:
+    /**
+     * @brief Defualt constructor
+     */
+    AUVControllerWithFF() : AUVBaseController(){}
+
+    /**
+     * @brief Deconstructor
+     */ 
+    ~AUVControllerWithFF() {}
+
+    /**
+     * @brief Implementation of controller
+     */ 
+    virtual void controllerRun(const AUVKineticSensor& sensor, const AUVControllerInput& input, AUVControllerOutput& output, const double dt) override;
+
+}; // AUVControllerWithFF
+};  // ns
+
+#endif
+
