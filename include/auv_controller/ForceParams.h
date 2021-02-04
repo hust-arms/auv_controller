0193c36c3b427c069406e6bf4d22864ba71af63c
wangzhao9562 <wangzhao9562@gmail.com>
2021-02-04 01:59:35 -0800

    Add force paramter structure in ForceParams.h

--- include/auv_controller/ForceParams.h
+++ include/auv_controller/ForceParams.h
@@ -21,7 +21,23 @@ namespace auv_controller{
             y_uudr_ = yuudr; z_uuds_ = zuuds; z_uudb_ = zuudb;
             m_uuds_ = muuds; m_uudb_ = muudb; n_uudr_ = nuudr;
         }
-    }; // CtrlForceParams
+    }; // CtrlForceParams for lateral rouder
+
+    struct XForceParams{
+        double y_dup_, y_dus_, y_dlp_, y_dls_;
+        double z_dup_, z_dus_, z_dlp_, z_dls_;
+        double m_dup_, m_dus_, m_dlp_, m_dls_;
+        double n_dup_, n_dus_, n_dlp_, n_dls_;
+
+        void setParameters(double ydup, double ydus, double ydlp, double ydls, double zdup, double zdus, double zdlp, double zdls,
+                           double mdup, double mdus, double mdlp, double mdls, double ndup, double ndus, double ndlp, double ndls){
+            y_dup_ = ydup; y_dus_ = ydus; y_dlp_ = ydlp; y_dls_ = ydls;
+            z_dup_ = zdup; z_dus_ = zdus; z_dlp_ = zdlp; z_dls_ = zdls;
+            m_dup_ = mdup; m_dus_ = mdus; m_dlp_ = mdlp; m_dls_ = mdls;
+            n_dup_ = ndup; n_dus_ = ndus; n_dlp_ = ndlp; n_dls_ = ndls;
+        }
+    };
+
 }; // ns
 
 #endif
