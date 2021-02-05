7e3d0e92e70e5bfdff7ca99bd578b34ac5150400
wangzhao9562 <wangzhao9562@gmail.com>
2021-02-04 20:19:51 -0800

    Add cmd parser for x type rudder model

--- test/AUVControllerTest.cpp
+++ test/AUVControllerTest.cpp
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "auv_controller_test_node");
 
-    bool with_ff, debug = false;
+    bool with_ff, x_type = false, debug = false;
 
     if(argc >= 2 && strcmp(argv[1], "no_ff") == 0){
         printf("Controller without front fins\n");
@@ -25,13 +25,21 @@ int main(int argc, char** argv)
         with_ff = true;
     }
 
-    if(argc == 3 && strcmp(argv[2], "debug") == 0){
+    if(argc >= 3 && strcmp(argv[2], "x") == 0){
+        printf("Controller for X type rudder\n");
+        x_type = true;
+    }
+    else{
+        printf("Controller for crosshead type rudder\n");
+    }
+
+    if(argc == 4 && strcmp(argv[3], "debug") == 0){
         printf("Debug mode\n");
         debug = true;
     }
 
     std::string auv_ns = "armsauv";
-    auv_controller::AUVControllerROS auv_ctrl(auv_ns, with_ff, debug);
+    auv_controller::AUVControllerROS auv_ctrl(auv_ns, with_ff, x_type, debug);
 
     auv_ctrl.startControl();
 
