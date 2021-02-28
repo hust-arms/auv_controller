d394e4bbee9a901cb7f6fe75fc51fbb744540db1
wangzhao9562 <wangzhao9562@gmail.com>
2021-02-28 22:35:49 +0800

    Update default PID parameters

---
+++ include/auv_controller/AUVPIDController.h
@@ -0,0 +1,174 @@
+/*
+ * Filename: AUVPIDController.h
+ * Path: auv_controller
+ * Created Date: Saturday, Faburary 27th 2021, 15:14:39         
+ * Author: zhao wang
+ * 
+ * Copyright (c) 2021 hust-arms
+ */
+
+#ifndef AUV_PID_CONTROLLER_H_
+#define AUV_PID_CONTROLLER_H_
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include "BodyParams.h"
+#include "PIDCtrlParams.h"
+#include "AUVCommon.h"
+
+#include "PIDController.h"
+
+#include <boost/shared_ptr.hpp>
+
+namespace auv_controller{
+
+typedef boost::shared_ptr<PIDController> PIDControllerPtr;
+
+/**
+ * @brief AUV controller input parameters
+ */
+struct AUVControllerInput{
+    /**
+     * @brief Constructor
+     */
+    AUVControllerInput(double depthd, double pitchd, double yawd, double xd, double yd, double ud) :
+        depth_d_(depthd), pitch_d_(pitchd), yaw_d_(yawd), x_d_(xd), y_d_(yd), u_d_(ud){};
+
+    double depth_d_; // desired depth
+    double pitch_d_; // desired pitch
+    double yaw_d_; // desired yaw
+    double x_d_; // waypoint on the desired line
+    double y_d_; // waypoint on the desired line
+    double u_d_; // desired linear x velocity
+}; //  AUVControllerInput
+
+/**
+  * @brief AUV controller output parameters
+  */
+struct AUVControllerOutput{
+    double rudder_;
+    double fwd_fin_;
+    double aft_fin_;
+    double rpm_;
+
+    double upper_p_; // for X type rudder
+    double upper_s_;
+    double lower_p_;
+    double lower_s_;
+}; // AUVControllerOutput
+
+
+/**
+ * @brief Base auv slide model controller 
+ */ 
+class AUVPIDController
+{
+public:
+    /**
+     * @brief Default class initialization
+     */
+    AUVPIDController();
+
+    /**
+     * @brief Deconstructor
+     */
+    virtual ~AUVPIDController() {} 
+
+    /**
+     * @brief Set AUV body parameters whicih includes mass, length, gravity, buoyancy et al
+     */
+    void setAUVBodyParams(double m, double l, double w, double b,
+        double xb, double yb, double zb, double xg, double yg, double zg,
+        double ixx, double iyy, double izz)
+    {
+        body_.setParameters(m, l, w, b, xb, yb, zb, xg, yg, zg, ixx, iyy, izz);
+    };
+
+    /**
+     * @brief Set AUV body parameters whicih includes mass, length, gravity, buoyancy et al
+     */
+    bool setAUVBodyParams(const std::vector<double>& body);
+
+    /**
+     * @brief Set control parameters
+     */
+    void setCtrlParams(double kpz, double kiz, double kdz,
+        double kptheta, double kitheta, double kdtheta,
+        double kpy, double kiy, double kdy,
+        double kppsi, double kipsi, double kdpsi);
+
+    /**
+     * @brief Set control parameters
+     */ 
+    bool setCtrlParams(const std::vector<double>& ctrl);
+    
+    /**
+     * @brief Set control parameters
+     */
+    void setCtrlParams(double kp, double ki, double kd, unsigned int flag);
+
+    /**
+     * @brief Control solution
+     */
+    virtual void controllerRun(const AUVKineticSensor& sensor, const AUVControllerInput& input, AUVControllerOutput& output, const double dt, bool vel_ctrl) = 0;
+
+    /**
+     * @brief Serialize auv body parameters
+     */
+    virtual void serializeAUVBodyParams(std::stringstream& str);
+    
+    /**
+     * @brief Serialize auv control parameters
+     */
+    virtual void serializeAUVControlParams(std::stringstream& str);
+
+protected:
+    /**
+     * @brief Initialize with default parameters
+     */
+    virtual void defaultInit();
+
+    /**
+     * @brief Filter function
+     */
+    int sign(double input){
+        if(input > 0){
+            return 1;
+        }
+        else if (input < 0){
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    };
+    
+    double sat(double input, double thick){
+        return fabs(input) >= thick ? sign(input) : (input / thick);
+    };
+
+
+protected:
+    PIDControllerPtr depth_controller_;
+    PIDControllerPtr latdev_controller_;
+    PIDControllerPtr vel_controller_;
+
+protected:
+    AUVBodyParams body_; 
+    AUVKinetic kinetic_;
+    PIDCtrlParams pid_ctrl_;
+    
+    // Commands
+    double deltab_, deltas_, deltar_;
+    double deltaup_, deltaus_, deltalp_, deltals_; // for X type rudder
+    
+    // Const value
+    const unsigned int body_num_ = 13;
+    const unsigned int ctrl_num_ = 12;
+}; // AUVPIDController
+}; // ns
+
+#endif
+
---
+++ src/AUVPIDController.cpp
@@ -0,0 +1,114 @@
+/*                                                                                                                                               
+ * Filename: AUVPIDController.cpp
+ * Path: auv_controller
+ * Created Date: Saturday, Faburary 27th 2021, 15:59:16
+ * Author: zhao wang
+ * 
+ * Copyright (c) 2021 Your Company
+ */
+
+#include <math.h>
+#include "auv_controller/AUVPIDController.h"
+
+namespace auv_controller{
+AUVPIDController::AUVPIDController(){
+    defaultInit();
+}
+
+
+bool AUVPIDController::setAUVBodyParams(const std::vector<double>& body){
+    if(body.size() == body_num_){
+        setAUVBodyParams(body[0], body[1], body[2], body[3], body[4], body[5], body[6],
+            body[7], body[8], body[9], body[10], body[11], body[12]);
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+void AUVPIDController::setCtrlParams(double kpz, double kiz, double kdz, double kptheta, double kitheta, double kdtheta,
+        double kpy, double kiy, double kdy, double kppsi, double kipsi, double kdpsi)
+{
+    pid_ctrl_.z_.setParameters(kpz, kiz, kdz); // depth
+    pid_ctrl_.theta_.setParameters(kptheta, kitheta, kdtheta); // pitch
+    pid_ctrl_.y_.setParameters(kpy, kiy, kdy); // y
+    pid_ctrl_.psi_.setParameters(kppsi, kipsi, kdpsi); // yaw
+};
+
+
+/**
+ * @brief Set control parameters
+ */
+bool AUVPIDController::setCtrlParams(const std::vector<double>& ctrl){
+    if(ctrl.size() == ctrl_num_){
+        setCtrlParams(ctrl[0], ctrl[1], ctrl[2], ctrl[3], ctrl[4], ctrl[5], ctrl[6], ctrl[7], ctrl[8], ctrl[9], ctrl[10], ctrl[11]);
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+/**
+ * @brief Set control parameters
+ */
+void AUVPIDController::setCtrlParams(double kp, double ki, double kd, unsigned int flag)
+{
+    switch (flag)
+    {
+    case 0:
+        pid_ctrl_.z_.setParameters(kp, ki, kd);
+        break;
+    case 1:
+        pid_ctrl_.theta_.setParameters(kp, ki, kd);
+        break;
+    case 2:
+        pid_ctrl_.y_.setParameters(kp, ki, kd);
+    case 3:
+        pid_ctrl_.psi_.setParameters(kp, ki, kd);
+        break;
+    default:
+        break;
+    }
+}
+
+
+/**
+ * @brief Initialize with default paramters
+ */
+void AUVPIDController::defaultInit(){
+    setAUVBodyParams(228.486, 3.145, 2239.163, 2295.142, 0, 0, 0, 1.64, 0, 0.015, 2.923, 150.3313, 150.3313);
+    setCtrlParams(0.03, 0.0, 5.0, 0.03, 0.0, 5.0, 0.03, 0.0, 5.0, 0.03, 0.0, 5.0); // x type rudder
+    //
+    depth_controller_ = boost::make_shared<PIDController>(pid_ctrl_.z_.kp_, pid_ctrl_.z_.ki_, pid_ctrl_.z_.kd_);
+    latdev_controller_ = boost::make_shared<PIDController>(pid_ctrl_.y_.kp_, pid_ctrl_.y_.ki_, pid_ctrl_.y_.kd_);
+    vel_controller_ = boost::make_shared<PIDController>(1000.0, 0.0, 0.0);
+
+    deltab_ = 0.0; deltas_ = 0.0; deltar_ = 0.0;
+    deltaup_ = 0.0; deltaus_ = 0.0; deltalp_ = 0.0; deltals_ = 0.0;
+}
+
+/**
+ * @brief Serialize auv body parameters
+ */ 
+void AUVPIDController::serializeAUVBodyParams(std::stringstream& str){
+    str << "m: " << body_.m_ << " l: " << body_.l_ << " w: " << body_.w_ << " b: " << body_.b_;
+    str << " xb: " << body_.x_b_ << " yb: " << body_.y_b_ << " zb: " << body_.z_b_;
+    str << " xg: " << body_.x_g_ << " yg: " << body_.y_g_ << " zg: " << body_.z_g_;
+    str << " ixx: " << body_.i_xx_ << " iyy: " << body_.i_yy_ << " izz: " << body_.i_zz_;
+}
+
+
+/**
+ * @brief Serialize auv control parameters
+ */
+void AUVPIDController::serializeAUVControlParams(std::stringstream& str){
+    str << "kpz: " << pid_ctrl_.z_.kp_ << " kiz: " << pid_ctrl_.z_.ki_ << " kdz: " << pid_ctrl_.z_.kd_;
+    str << " kptheta: " << pid_ctrl_.theta_.kp_ << " kitheta: " << pid_ctrl_.theta_.ki_ << " kdtheta: " << pid_ctrl_.theta_.kd_;
+    str << " kpy: " << pid_ctrl_.y_.kp_ << " kiy: " << pid_ctrl_.y_.ki_ << " kdy: " << pid_ctrl_.y_.kd_;
+    str << " kppsi: " << pid_ctrl_.psi_.kp_ << " kipsi: " << pid_ctrl_.psi_.ki_ << " kdpsi: " << pid_ctrl_.psi_.kd_;
+}
+
+}; // ns
+
