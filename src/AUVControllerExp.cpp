ea18b98f2d7e237bdc1b712f1f2f46a6ee7c261b
wangzhao9562 <wangzhao9562@gmail.com>
2021-03-29 14:55:49 +0800

    Revise cotroller input

--- src/AUVControllerExp.cpp
+++ src/AUVControllerExp.cpp
@@ -287,7 +287,7 @@ void AUVControllerExp::controlThread(){
         }
 
         // Create Controller input
-        AUVControllerInput input(getDesiredDepth(), -getDesiredPitch(), -getDesiredYaw(), -getDesiredX(), -getDesiredY(), getDesiredLinVelX());
+        AUVControllerInput input(getDesiredDepth(), -getDesiredPitch(), -getDesiredYaw(), getDesiredX(), -getDesiredY(), getDesiredLinVelX());
 
         // Print control input
         if(debug_){
